cdffc35455ad8650d9890019e0052afc7454e34e
Christopher Moore <gkchristopher@icloud.com>
2018-11-12 19:22:43 -0500

    fix example project for graph updates

--- GraphViewExample/GraphViewExample/ViewController.swift
+++ GraphViewExample/GraphViewExample/ViewController.swift
@@ -10,30 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var graphView: GraphView!
+    private var points = [CGFloat]()
+
+    @IBOutlet var graphView: GraphView! {
+        didSet {
+            graphView.dataSource = self
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        points = (0..<10).map { _ in CGFloat.random(in: 0.0...1.0) }
+
         graphView.tintColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
         graphView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         graphView.labelFillColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        graphView.lineLabelColor = .black
+        graphView.gridLineColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
 
-        graphView.maxY = 5
-        graphView.minY = 0
-        graphView.padding = UIEdgeInsetsMake(20, 20, 20, 20)
-        graphView.data = [GraphPoint(label: "0", value: 0, axisLabel: NSAttributedString(string: "1")),
-                          GraphPoint(label: "1", value: 1, axisLabel: NSAttributedString(string: "2")),
-                          GraphPoint(label: "4", value: 4, axisLabel: NSAttributedString(string: "3")),
-                          GraphPoint(label: "3", value: 3, axisLabel: NSAttributedString(string: "4")),
-                          GraphPoint(label: "1", value: 1, axisLabel: NSAttributedString(string: "5")),
-                          GraphPoint(label: "5", value: 5, axisLabel: NSAttributedString(string: "6")),
-                          GraphPoint(label: "1", value: 1, axisLabel: NSAttributedString(string: "7")),
-                          GraphPoint(label: "3", value: 3, axisLabel: NSAttributedString(string: "8")),
-                          GraphPoint(label: "2", value: 2, axisLabel: NSAttributedString(string: "9")),
-                          GraphPoint(label: "1", value: 1, axisLabel: NSAttributedString(string: "10")),
-                          GraphPoint(label: "4", value: 4, axisLabel: NSAttributedString(string: "11"))]
+        graphView.padding = UIEdgeInsets.init(top: 20, left: 20, bottom: 20, right: 20)
     }
 }
 
+extension ViewController: GraphViewDataSource {
+
+    func numberOfPoints(in graphView: GraphView) -> Int {
+        return points.count
+    }
+    
+    func graphView(_ graphView: GraphView, valueAt index: Int) -> CGFloat {
+        return points[index]
+    }
+}
